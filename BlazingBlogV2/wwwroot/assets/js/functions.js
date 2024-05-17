3aeeea4ebb64d2e055e7fb59a05988e363c6fbd5
Felly Ngoy <FellyN@mintek.co.za>
2024-05-17 10:02:38 +0200

    Final Initial Project setup

--- BlazingBlogV2/wwwroot/assets/js/functions.js
+++ BlazingBlogV2/wwwroot/assets/js/functions.js
@@ -3,5 +3,27 @@
 //    $(window).scroll(function(){
 //    $('.topnav').toggleClass('scrollednav py-0', $(this).scrollTop() > 50);
 //    });
-    
+
 //});
+
+document.addEventListener("DOMContentLoaded", _ => {
+    const topNav = document.getElementsByClassName("topnav")[0];
+    if (topNav) {
+        window.scroll = () => {
+            if (window.scorllY > 50) {
+                //add these classes scroll py-0
+                topNav.classList.add('scrollednav', 'py-0')
+            }
+
+            else {
+                //remove these classes scroll py-0
+                topNav.classList.remove('scrollednav', 'py-0')
+            }
+        };
+    }
+});
+
+function toggleMenu(e) {
+    e.target.classlist.toggle('collapsed');
+    const navbarWrapper = document.getElementById('top-navbar-menu-wrapper').classList.toggle('show');
+}
